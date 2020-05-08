e214fb36dd811cbeb9833a3f6c1fb0d570e29a91
M5A99X <M5A99X@Edward-Cadet-PC>
2020-05-08 13:02:31 -0400

    Requete "afficher consultation par docteur" fait.

--- src/DAO/ConsultationDAO.java
+++ src/DAO/ConsultationDAO.java
@@ -5,10 +5,43 @@
  */
 package DAO;
 
+import controleur.HibernateUtil;
+import entite.Consultation;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+
 /**
  *
  * @author M5A99X
  */
 public class ConsultationDAO extends DAO {
     
+    // Méthode pour afficher le nombre de consultations par docteur
+    public static void consultParDocteurs() {
+        // ouvre la session
+        session = HibernateUtil.getSessionFactory().openSession();
+        
+        // crée le critère avec la table Consultation comme base
+        Criteria cr = session.createCriteria(Consultation.class);
+        
+        // crée la requête qui va chercher le nombre de consultations par docteur
+        // avec un group by sur docteur.matricule, docteur.nomm, docteur.nomm
+        Query query = session.createQuery("SELECT docteur.matricule, docteur.nomm, docteur.prenomm, count(DISTINCT c.docteur) "
+                        + "FROM Consultation c join c.docteur docteur "
+                        + "GROUP BY docteur.matricule, docteur.nomm, docteur.prenomm");
+        
+        
+        
+        // contient la liste des résultats
+        List<Object[]> resultats = query.list();
+        
+        // boucle qui affiche chacun des résultats
+        for(Object[] element : resultats) {
+            System.out.println("matricule: " + element[0] + " - nom: " + element[1] + " - prenom: " + element[2] + " - consultations: " + element[3]);
+        }
+        
+        // ferme la session
+        session.close();           
+    }
 }
