a8b7bf8e3e2a450880781a23bdcf55b4d12bae65
Dominic Tardif <1995622@bdeb.qc.ca>
2020-05-07 22:37:55 -0400

    Refactoring pour inclure les annotations et continuation de l'insertion de données. Insertion complète.

--- SQL/PharmacieDML.sql
+++ SQL/PharmacieDML.sql
@@ -58,16 +58,16 @@ INSERT INTO ordonnancechirurgie (numOrd, rang, nomChirurgie)
 VALUES(10075182,2,'');
 
 
-INSERT INTO ordonnancemedicaments (numOrd, id, nbBoites)
-VALUES (50690480,02380617,1);
-INSERT INTO ordonnancemedicaments (numOrd, id, nbBoites)
-VALUES (50000345,00596612,1);
-INSERT INTO ordonnancemedicaments (numOrd, id, nbBoites)
-VALUES (50032780,02230897,1);
-INSERT INTO ordonnancemedicaments (numOrd, id, nbBoites)
-VALUES (50297401,00556734,2);
-INSERT INTO ordonnancemedicaments (numOrd, id, nbBoites)
-VALUES (50002504,00878200,1);
+INSERT INTO ordonnancemedicaments (numOrd, idMed, nbBoites)
+VALUES (50690480,'02380617',1);
+INSERT INTO ordonnancemedicaments (numOrd, idMed, nbBoites)
+VALUES (50000345,'00596612',1);
+INSERT INTO ordonnancemedicaments (numOrd, idMed, nbBoites)
+VALUES (50032780,'02230897',1);
+INSERT INTO ordonnancemedicaments (numOrd, idMed, nbBoites)
+VALUES (50297401,'00556734',2);
+INSERT INTO ordonnancemedicaments (numOrd, idMed, nbBoites)
+VALUES (50002504,'00878200',1);
 
 
 INSERT INTO docteur (matricule, nomM, prenomM, specialite, ville, adresse, niveau, nbrPatients)
@@ -93,46 +93,75 @@ VALUES(94526387,'Gravel','Laurence',48,'Laval','500 Avenue Landry','Docteur',25)
 
 
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Michel','M','24062501',TO_DATE('24-06-1925','DD-MM-YYYY'),TO_DATE('05-03-2020','DD-MM-YYYY'),59630145); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(1325969751,'Proulx','Michel','M','24062501',TO_DATE('24-06-1925','DD-MM-YYYY'),TO_DATE('05-03-2020','DD-MM-YYYY'),59630145);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Laurent','M','27097501',TO_DATE('27-09-1975','DD-MM-YYYY'),TO_DATE('15-03-2020','DD-MM-YYYY'),16482375); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(1635249856,'Bédard','Laurent','M','27097501',TO_DATE('27-09-1975','DD-MM-YYYY'),TO_DATE('15-03-2020','DD-MM-YYYY'),16482375);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Sophia','F','09071601',TO_DATE('09-07-2016','DD-MM-YYYY'),TO_DATE('25-03-2020','DD-MM-YYYY'),60314967); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(1965784256,'Blais','Sophia','F','09071601',TO_DATE('09-07-2016','DD-MM-YYYY'),TO_DATE('25-03-2020','DD-MM-YYYY'),60314967);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Damien','M','29010101',TO_DATE('29-01-2001','DD-MM-YYYY'),TO_DATE('18-02-2020','DD-MM-YYYY'),34695210); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(2046589631,'Couture','Damien','M','29010101',TO_DATE('29-01-2001','DD-MM-YYYY'),TO_DATE('18-02-2020','DD-MM-YYYY'),34695210);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Yvette','F','20046901',TO_DATE('20-04-1969','DD-MM-YYYY'),TO_DATE('28-02-2020','DD-MM-YYYY'),86419562); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(2415963788,'Cyr','Yvette','F','20046901',TO_DATE('20-04-1969','DD-MM-YYYY'),TO_DATE('28-02-2020','DD-MM-YYYY'),86419562);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Myriam','F','26129901',TO_DATE('26-12-1999','DD-MM-YYYY'),TO_DATE('14-01-2020','DD-MM-YYYY'),54723810); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(2679854130,'Dion','Myriam','F','26129901',TO_DATE('26-12-1999','DD-MM-YYYY'),TO_DATE('14-01-2020','DD-MM-YYYY'),54723810);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Romuald','M','28025301',TO_DATE('28-02-1953','DD-MM-YYYY'),TO_DATE('25-01-2020','DD-MM-YYYY'),73269421); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(3652154789,'Mercier','Romuald','M','28025301',TO_DATE('28-02-1953','DD-MM-YYYY'),TO_DATE('25-01-2020','DD-MM-YYYY'),73269421);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Alain','M','17073601',TO_DATE('17-07-1936','DD-MM-YYYY'),TO_DATE('22-01-2020','DD-MM-YYYY'),59630145); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(3125698541,'Bolduc','Alain','M','17073601',TO_DATE('17-07-1936','DD-MM-YYYY'),TO_DATE('22-01-2020','DD-MM-YYYY'),59630145);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Marie-Anne','F','22108301',TO_DATE('22-10-1983','DD-MM-YYYY'),TO_DATE('30-01-2020','DD-MM-YYYY'),29671548); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(3025690185,'Fortier','Marie-Anne','F','22108301',TO_DATE('22-10-1983','DD-MM-YYYY'),TO_DATE('30-01-2020','DD-MM-YYYY'),29671548);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Claude','M','16040801',TO_DATE('16-04-2008','DD-MM-YYYY'),TO_DATE('08-04-2020','DD-MM-YYYY'),94526387); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(4201569389,'Hamel','Claude','M','16040801',TO_DATE('16-04-2008','DD-MM-YYYY'),TO_DATE('08-04-2020','DD-MM-YYYY'),94526387);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Sabrina','F','08108801',TO_DATE('08-10-1988','DD-MM-YYYY'),TO_DATE('18-04-2020','DD-MM-YYYY'),42103690); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(4632589610,'Germain','Sabrina','F','08108801',TO_DATE('08-10-1988','DD-MM-YYYY'),TO_DATE('18-04-2020','DD-MM-YYYY'),42103690);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Nicolas','M','05067401',TO_DATE('05-06-1974','DD-MM-YYYY'),TO_DATE('13-04-2020','DD-MM-YYYY'),16482375); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(4698521307,'Lemay','Nicolas','M','05067401',TO_DATE('05-06-1974','DD-MM-YYYY'),TO_DATE('13-04-2020','DD-MM-YYYY'),16482375);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Sophie','F','18129001',TO_DATE('18-12-1990','DD-MM-YYYY'),TO_DATE('09-02-2020','DD-MM-YYYY'),86419562); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(5132964520,'Villeneuve','Sophie','F','18129001',TO_DATE('18-12-1990','DD-MM-YYYY'),TO_DATE('09-02-2020','DD-MM-YYYY'),86419562);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Claudette','F','21120401',TO_DATE('21-12-2004','DD-MM-YYYY'),TO_DATE('01-04-2020','DD-MM-YYYY'),54723810); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(5426978162,'Larouche','Claudette','F','21120401',TO_DATE('21-12-2004','DD-MM-YYYY'),TO_DATE('01-04-2020','DD-MM-YYYY'),54723810);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Jacques','M','30084501',TO_DATE('30-08-1945','DD-MM-YYYY'),TO_DATE('10-02-2020','DD-MM-YYYY'),73269421); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(6130254987,'Moreau','Jacques','M','30084501',TO_DATE('30-08-1945','DD-MM-YYYY'),TO_DATE('10-02-2020','DD-MM-YYYY'),73269421);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Martine','F','24115501',TO_DATE('24-11-1955','DD-MM-YYYY'),TO_DATE('07-03-2020','DD-MM-YYYY'),34695210); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(6485936980,'Bertrand','Martine','F','24115501',TO_DATE('24-11-1955','DD-MM-YYYY'),TO_DATE('07-03-2020','DD-MM-YYYY'),34695210);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Marjolaine','F','13099601',TO_DATE('13-09-1996','DD-MM-YYYY'),TO_DATE('14-02-2020','DD-MM-YYYY'),29671548); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(7215489645,'Lacroix','Marjolaine','F','13099601',TO_DATE('13-09-1996','DD-MM-YYYY'),TO_DATE('14-02-2020','DD-MM-YYYY'),29671548);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Martin','M','01077901',TO_DATE('01-07-1979','DD-MM-YYYY'),TO_DATE('22-01-2020','DD-MM-YYYY'),60314967); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(7398754610,'Gélinas','Martin','M','01077901',TO_DATE('01-07-1979','DD-MM-YYYY'),TO_DATE('22-01-2020','DD-MM-YYYY'),60314967);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Amélie','F','26048601',TO_DATE('26-04-1986','DD-MM-YYYY'),TO_DATE('24-04-2020','DD-MM-YYYY'),42103690); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(8462597568,'Pépin','Amélie','F','26048601',TO_DATE('26-04-1986','DD-MM-YYYY'),TO_DATE('24-04-2020','DD-MM-YYYY'),42103690);
 INSERT INTO dossierpatient (numDos, nomP, prenomP, genre, numAS, dateNaiss, dateC, matricule)
-VALUES(,'','Rodrigue','M','15106801',TO_DATE('15-10-1968','DD-MM-YYYY'),TO_DATE('03-05-2020','DD-MM-YYYY'),94526387); --(VAR(16), VAR(30), VAR(30), ('M', 'F'), VAR(12), DATE, DATE, VAR(8))
+VALUES(8634512059,'Giguère','Rodrigue','M','15106801',TO_DATE('15-10-1968','DD-MM-YYYY'),TO_DATE('03-05-2020','DD-MM-YYYY'),94526387);
 
 
 INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
-VALUES(,,TO_DATE('','DD-MM-YYYY'),'',);   --(VAR(8), VAR(16), DATE, VAR(255), NUM(8))
+VALUES(59630145,3125698541,TO_DATE('23-01-2020','DD-MM-YYYY'),'',50000345);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(94526387,8634512059,TO_DATE('26-01-2020','DD-MM-YYYY'),'',10075182);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(86419562,5132964520,TO_DATE('08-01-2020','DD-MM-YYYY'),'',50032780);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(42103690,4632589610,TO_DATE('15-02-2020','DD-MM-YYYY'),'',10961257);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(59630145,1325969751,TO_DATE('20-03-2020','DD-MM-YYYY'),'',50690480);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(34695210,6485936980,TO_DATE('23-04-2020','DD-MM-YYYY'),'',10358961);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(86419562,2415963788,TO_DATE('09-03-2020','DD-MM-YYYY'),'',10004895);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(29671548,7215489645,TO_DATE('16-02-2020','DD-MM-YYYY'),'',50002504);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(73269421,6130254987,TO_DATE('04-12-2020','DD-MM-YYYY'),'',50297401);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(34695210,2046589631,TO_DATE('03-03-2020','DD-MM-YYYY'),'',10125746);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+
+VALUES(34695210,2046589631,TO_DATE('18-03-2020','DD-MM-YYYY'),'',10125746);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(42103690,4632589610,TO_DATE('01-03-2020','DD-MM-YYYY'),'',10961257);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(34695210,6485936980,TO_DATE('28-04-2020','DD-MM-YYYY'),'',10358961);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(34695210,6485936980,TO_DATE('07-05-2020','DD-MM-YYYY'),'',10358961);
+INSERT INTO consultation (CodeDocteur, numDos, dateC, diagnostic, numOrd)
+VALUES(94526387,8634512059,TO_DATE('26-02-2020','DD-MM-YYYY'),'',10075182);
