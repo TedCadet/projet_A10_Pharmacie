56f67639a9c6b2bae5dbf3445fb6cdf22ec745ef
M5A99X <M5A99X@Edward-Cadet-PC>
2020-05-08 20:21:13 -0400

    Rajout des insertions de données dans AppCtr. Presque fini.

--- src/controleur/AppCtr.java
+++ src/controleur/AppCtr.java
@@ -47,54 +47,59 @@ public class AppCtr {
             sessionFactory = HibernateUtil.getSessionFactory();
             
             //code pour insérer un enregistrement par table
-            //code test pour insérer une donnée "Médicament" dans le tableau
+            //une donnée "Médicament" dans le tableau
             Medicament medTest = new Medicament("02222222", new Categories((byte) 8, "catTest"), "Test", new BigDecimal(22), null);
-            DAO.delete(medTest);
-            DAO.insert(medTest);
             
-            //code test pour insérer une donnée "Categorie" dans le tableau
-            Categories catTest = new Categories((byte) 2, "catTest", "test d'insertion", null);
-            DAO.delete(catTest);
-            DAO.insert(catTest);
+            // une donnée "Categorie" dans le tableau
+            Categories catTest = new Categories((byte) 2, "catTest", "test d'insertion", null); 
             
             //code test pour insérer une donnée "Ordonnance" dans le tableau 
             Ordonnance ord1Test = new Ordonnance(10352222, null, "chirurgie", new Date(), null, null, null);
-            DAO.delete(ord1Test);
-            DAO.insert(ord1Test);
-            
-            Ordonnance ord2Test = new Ordonnance(10352299, null, "medicament", new Date(), null, null, null);
-            //DAO.delete(ord2Test);
-            //DAO.insert(ord2Test);
-            
-            //code test pour insérer une donnée "Docteur" dans le tableau
-            Docteur docTest = new Docteur(29672222, null, "nomTest", "prenomTest", "villeTest", "adresseTest", "Interne", (short) 22, null, null);
-            //DAO.delete(docTest); 
-            //DAO.insert(docTest);
-            
-            //code test pour insérer une donnée "Dossierpatient" dans le tableau
-            Dossierpatient dosPatTest = new Dossierpatient((long) 22, null, "nomTest", "prenomTest", 'M', "TEST2222", null, null, null);
-            //DAO.delete(dosPatTest);
-            //DAO.insert(dosPatTest);
-            
+ 
+            Ordonnance ord2Test = new Ordonnance(10222299, null, "medicaments", new Date(), null, null, null);
+
             //code test pour insérer une donnée "Specialite" dans le tableau
             Specialite specTest = new Specialite((byte) 22, "titreTest", "descriptionTest", null);
-            DAO.delete(specTest);
-            DAO.insert(specTest);
-            
-            //code test pour insérer une donnée "Ordonnancechirurgie" dans le tableau ** fonctionne pas
-            Ordonnancechirurgie ordChiTest = new Ordonnancechirurgie(new OrdonnancechirurgieId(10352222, (short) 2), null, "cirurgieTest");
-            //DAO.delete(ordChiTest);
-            //DAO.insert(ordChiTest);
-            
-            //code test pour insérer une donnée "Ordonnancemedicaments" dans le tableau ** fonctionne pas
-            Ordonnancemedicaments ordMedTest = new Ordonnancemedicaments(new OrdonnancemedicamentsId(10352299, "02222222"), null, null, (short) 22);
-            //DAO.delete(ordMedTest);
-            //DAO.insert(ordMedTest);
             
             //code test pour insérer une donnée "Consultation" 
             Consultation conTest = new Consultation(new ConsultationId(29672222, (long) 22, new Date()), null, null, null, "DiagnoticsTest");
-            //DAO.delete(conTest);
-            //DAO.insert(conTest);
+
+            //code test pour insérer une donnée "Docteur" dans le tableau
+            Docteur docTest = new Docteur(29672222, null, "nomTest", "prenomTest", "villeTest", "adresseTest", "Interne", (short) 22, null, null);
+
+            //code test pour insérer une donnée "Dossierpatient" dans le tableau
+            Dossierpatient dosPatTest = new Dossierpatient((long) 22, null, "nomTest", "prenomTest", 'M', "TEST2222", null, null, null);
+
+            //code test pour insérer une donnée "Ordonnancechirurgie" dans le tableau ** fonctionne pas
+            Ordonnancechirurgie ordChiTest = new Ordonnancechirurgie(new OrdonnancechirurgieId(10352222, (short) 2), null, "cirurgieTest");
+
+            //code test pour insérer une donnée "Ordonnancemedicaments" dans le tableau ** fonctionne pas
+            Ordonnancemedicaments ordMedTest = new Ordonnancemedicaments(new OrdonnancemedicamentsId(10352299, "02222222"), null, null, (short) 22);     
+            
+            // delete
+//            DAO.delete(dosPatTest);
+//            DAO.delete(docTest);
+//            DAO.delete(ordChiTest);
+//            DAO.delete(ordMedTest);
+//            DAO.delete(medTest);
+//            DAO.delete(ord1Test);
+//            DAO.delete(ord2Test);
+//            DAO.delete(specTest);
+//            DAO.delete(catTest);
+//            DAO.delete(conTest);
+   
+            // insertion
+            DAO.insert(catTest);
+            DAO.insert(specTest);
+            DAO.insert(ord2Test);
+            DAO.insert(ord1Test);
+            DAO.insert(medTest);
+            DAO.insert(ordMedTest);
+            DAO.insert(ordChiTest);
+            DAO.insert(docTest);
+            DAO.insert(dosPatTest);
+            DAO.insert(conTest);
+
             
             //code pour appeler les requêtes (static) du DAO
             Map<Docteur,Long> chirurgiesParDocteur = DocteurDAO.chirurgiesPrescritesParDocteur();
